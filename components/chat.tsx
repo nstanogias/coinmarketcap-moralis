1c2fe1ab0c53f30472ab17d2e19a30a54f82951d
nstanogias <nstanogias@gmail.com>
2022-05-26 10:25:57 +0200

    added decentralized chat functionality

--- components/chat.tsx
+++ components/chat.tsx
@@ -5,6 +5,8 @@ import ChatCard from './ChatCard'
 import Button from './Button'
 import shiba from '../assets/shiba.png'
 import Image from 'next/image'
+import { faker } from '@faker-js/faker'
+import { GunContext } from '../context/gunContext'
 
 const styles = {
   bullishLabel: `flex cursor-pointer active:bg-green-600 items-center text text-green-600 border border-green-600 h-min px-2 rounded-lg`,
@@ -25,7 +27,48 @@ const Chat = () => {
   const [message, setMessage] = useState('')
   const [bullishValue, setBullishValue] = useState(true)
 
-  const sendMessage = () => {}
+  // @ts-ignore
+  const { gun, getMessages, state } = useContext(GunContext)
+
+  useEffect(() => {
+    getMessages('GUN_REF_7')
+  }, [])
+
+  const formattedMessagesArray = () => {
+    const uniqueArray = state.messages.filter((value, index) => {
+      const _value = JSON.stringify(value)
+
+      return (
+        index ===
+        state.messages.findIndex((obj) => {
+          return JSON.stringify(obj) === _value
+        })
+      )
+    })
+    console.log(uniqueArray)
+    return uniqueArray
+  }
+
+  const sendMessage = () => {
+    if (message.trim() === '') return
+
+    const messagesRef = gun.get('GUN_REF_7')
+
+    const newMessage = {
+      sender: faker.name.findName(),
+      avatar:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFIW5fZ3vSN6yGpD-w-6SsL2_ZPA_sw&usqp=CAU',
+      content: message.trim(),
+      isBullish: bullishValue,
+      createdAt: Date().substring(4, 11),
+      messageId: Date.now(),
+    }
+
+    console.log(newMessage)
+
+    messagesRef.set(newMessage)
+    setMessage('')
+  }
 
   return (
     <>
@@ -98,6 +141,21 @@ const Chat = () => {
       <div className={styles.postButtonContainer}>
         <Button label="Post" onPress={sendMessage} />
       </div>
+      {formattedMessagesArray()
+        .slice(0)
+        .reverse()
+        .map((message, index) => (
+          <ChatCard
+            key={index}
+            sender={message.sender}
+            senderAvatar="https:/encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFIW5fZ3vSN6yGpD-w-6SsL2_ZPA_sw&usqp=CAU"
+            bullish={message.isBullish}
+            timestamp={message.createdAt}
+            content={message.content}
+            likes="2.7K"
+            comments="19K"
+          />
+        ))}
     </>
   )
 }
